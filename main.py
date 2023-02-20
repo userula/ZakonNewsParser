6b5cf7f267cadddfb100eb142fe252940ab5ade9
ULAN BUZHBANOV <66116935+userula@users.noreply.github.com>
2023-02-20 16:07:22 +0600

    PEP-8

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ from bs4 import BeautifulSoup as BS
 
 tdate = datetime.date.today()
 
-def getTodaysNews():
-    h = {
+def get_todays_news():
+    headers = {
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"
     }
     # Get news from api
-    req = requests.get("https://www.zakon.kz/api/all-news-ajax/?pn=1&pSize=100", headers=h)
+    response = requests.get("https://www.zakon.kz/api/all-news-ajax/?pn=1&pSize=100", headers=headers)
     # Convert into json
-    data = json.loads(req.text)
+    data = json.loads(response.text)
     # Create list for storage links of each news
     news = []
     # Get all links of news
@@ -27,55 +27,53 @@ def getTodaysNews():
                 news.append(link.get("alias"))
 
     # Get info from news with sending a links
-    total = getNewsInfo(news)
+    total = get_news_info(news)
 
     # Create/write JSON file to storage data
     with open("data.json", "w", encoding='utf-8') as file:
         json.dump(total, file, indent=4, ensure_ascii=False)
 
 
-def getNewsInfo(news_list):
-    returnData = []
+def get_news_info(news_list):
+    return_data = []
     for link in news_list:
         # Send request to each link of news and get html content of page
         r = requests.get(f'https://www.zakon.kz/{link}')
         html = BS(r.content, "html.parser")
 
         # Start to find each needed element from html
-        articleBlock = html.find("div", "articleBlock")
+        article_block = html.find("div", "articleBlock")
 
-        title = articleBlock.select("h1")[0].text
-        publishedDate = articleBlock.select(".date")[0].text
-        comments = articleBlock.select(".comments")
+        title = article_block.select("h1")[0].text
+        published_date = article_block.select(".date")[0].text
+        comments = article_block.select(".comments")
         # Let's count the comments number, but as there were not any comments it will be always zero
-        commentsCounter = 0
+        comments_counter = 0
         for el in comments:
             if el.select(".title") is not None:
-                commentsCounter += 1
+                comments_counter += 1
 
         # To get all content from article, we had to find all tags from content class for example due to different font
-        c = articleBlock.find("div", class_="content").findAll(['p', 'a', 'span', 'blockquote'])
+        c = article_block.find("div", class_="content").findAll(['p', 'a', 'span', 'blockquote'])
         content = ""
         for e in c:
             content += e.text
 
         # Collect all article infos into one
-        returnData.append({
+        return_data.append({
             "Заголовок: ": title,
             "Контент:": content,
-            "Дата публикации:": publishedDate,
-            "Количество комментариев:": commentsCounter
+            "Дата публикации:": published_date,
+            "Количество комментариев:": comments_counter
         })
 
-    return returnData
+    return return_data
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     try:
-        getTodaysNews()
+        get_todays_news()
         print("Success!")
     except:
         print("Something went wrong")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
